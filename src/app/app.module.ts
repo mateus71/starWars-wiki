534cba6beee61926166fa07c2078bfc55d8f9ea8
mateus <201598>
2020-05-04 11:34:23 -0300

    'sidenav_commit_1'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetsModule } from './planets/planets.module';
-
-import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './templates/header/header.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavComponent } from './templates/side-nav/side-nav.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +26,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     PlanetsModule,
     HttpClientModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatSidenavModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
